faa25c36848a7046ed314969d411648779e79f44
RozierT <135091431+RozierT@users.noreply.github.com>
2023-11-10 18:01:20 -0800

    finished todos but still getting an error when trying to run

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,15 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+const db = await openDB('jate', 1);
+await db.put('jate', content);
+console.log('content added to the database');
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+const db = await openDB('jate', 1);
+const content = await db.getAll();
+};
 
 initdb();
